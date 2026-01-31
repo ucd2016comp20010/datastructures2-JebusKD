006c6d50558132584fcc0a4595cb91afec79aff1
Jamie Kildea <jamiekildea@hotmail.com>
2026-01-31 21:23:27 +0000

    CircularlyLinkedList add fix

--- src/project20280/list/CircularlyLinkedList.java
+++ src/project20280/list/CircularlyLinkedList.java
@@ -67,10 +67,8 @@ public class CircularlyLinkedList<E> implements List<E>, Iterable<E> {
         }
         else {
         	Node<E> curNode = this.tail;
-        	int targetIndex;
-        	if (i == 0) targetIndex = this.size;
-        	else targetIndex = i % this.size;
-        	for (int j = 1; j < i % this.size; j++) {
+        	int index = i % this.size;
+        	for (int j = 0; j < index; j++) {
         		curNode = curNode.getNext();
         	}
         	Node<E> newNode = new Node<E>(e, curNode.getNext());
