321fa51d0bdd1d25ca125555bde8bb6aac13f453
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-05 08:17:01 +0000

    ArrayDeque methods

--- src/project20280/stacksqueues/ArrayDeque.java
+++ src/project20280/stacksqueues/ArrayDeque.java
@@ -1,7 +1,43 @@
 package project20280.stacksqueues;
 
+import project20280.interfaces.Deque;
+
 public class ArrayDeque<E> implements Deque<E> {
 
+	public ArrayDeque() {
+		
+	}
 	
+	public void addFirst(E e) {
+		
+	}
+	
+	public void addLast(E e) {
+		
+	}
+	
+	public E removeFirst() {
+		
+	}
+	
+	public E removeLast() {
+		
+	}
+	
+	public E first() {
+		
+	}
+	
+	public E last() {
+		
+	}
+	
+	public E size() {
+		
+	}
+	
+	public E isEmpty() {
+		
+	}
 	
 }
