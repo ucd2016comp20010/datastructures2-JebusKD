2a29677b8e6d6d892e7156343d5c336aa9963830
Jamie Kildea <jamiekildea@hotmail.com>
2026-03-26 15:47:15 +0000

    simple solve

---
+++ src/project20280/exercises/leetcode658/Solution4.java
@@ -0,0 +1,20 @@
+package project20280.exercises.leetcode658;
+/* Non-heap solution that beats 56% on runtime and  29% memory */
+class Solution4 {
+	class Solution {
+	    int winstart,
+	    i;
+
+	    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+	        winstart = 0;
+	        for (i = k; i < arr.length && ((arr[i] == arr[winstart]) || (Math.abs(arr[i] - x) < Math.abs(arr[winstart] - x))); i++)
+	            winstart++;
+	        
+	        ArrayList<Integer> res = new ArrayList<Integer>(k);
+	        for (i = 0; i < k; i++)
+	            res.addLast(arr[winstart+i]);
+	        
+	        return res;
+	    }
+	}
+}
