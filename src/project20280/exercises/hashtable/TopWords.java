6c4f7ccb99cbdf7026d514724be445f6a15ada7d
Jamie Kildea <jamiekildea@hotmail.com>
2026-04-07 07:35:06 +0100

    topwords

--- src/project20280/exercises/hashtable/TopWords.java
+++ src/project20280/exercises/hashtable/TopWords.java
@@ -2,13 +2,15 @@ package project20280.exercises.hashtable;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import project20280.hashtable.ChainHashMap;
+import project20280.interfaces.Entry;
 
 public class TopWords {
 	public static void main(String []args) throws FileNotFoundException {
-		 File f = new File("sample_text.txt"); // check the path to the file
+		 File f = new File("C:/Users/jamie/Documents/College Stuff/Stage 2/Spring/COMP20280 Data Structures/Labs/Week 8/sample_text.txt"); // check the path to the file
 		 ChainHashMap<String, Integer> counter = new ChainHashMap<String, Integer>();
 		
 		 // use a Scanner to read words from the file
@@ -22,10 +24,28 @@ public class TopWords {
 			 // otherwise, find the entry for this word and increment by 1
 			 if ((val = counter.get(word)) != null) {
 				counter.put(word, val+1); 
+			 } else {
+				counter.put(word, 1); 
 			 }
 		 }
+		 
+		 scanner.close();
 		
 		 // sort the key, values...
 		 // Can you sort the Entries by the value?
-	 }
+		 Iterable<Entry<String, Integer>> entries = counter.entrySet();
+		 ArrayList<Entry<String, Integer>> sorted = new ArrayList<Entry<String, Integer>>();
+		 
+		 for (Entry<String, Integer> e : entries) {
+			 sorted.addLast(e);
+		 }
+		 
+		 sorted.sort((e1, e2) -> e1.getValue() - e2.getValue());
+		 
+		 for (int i = 1; i <= 10; i++) {
+			 Entry<String, Integer> e = sorted.get(sorted.size() - i);
+			System.out.println(e.getKey() + " : " + e.getValue());
+		 }
+	}
+	
 }
